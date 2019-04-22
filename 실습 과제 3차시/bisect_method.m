8dc3fc52880ce7bb4b99b550211f7cb8210a7206
서준원 <vegarian@overcast.kr>
2019-04-23 00:06:17 +0900

    error fixed

--- 실습 과제 3차시/bisect_method.m
+++ 실습 과제 3차시/bisect_method.m
@@ -1,5 +1,5 @@
 %% 0. 이분법을 활용한 방정식의 해 찾기 함수
-function [root, err, no_iter] = bisect_method(f, x1, x2, max_err)
+function [root, err, no_iter] = bisect_method(x, x1, x2, max_err)
 %% 1. 선언 및 입력
 syms x;
 f(x) = f;
@@ -14,11 +14,12 @@ if f(x1)*f(xmid)<0 % x1과 중간값이 이분법 사용 가능할때
 elseif f(xmid)*f(x2)<0 % x1과 중간값이 불가능하면 x2와 중간값 비교
     x1 = xmid; % x1을 기존 중간값으로 대체
 elseif (f(xmid)*f(x2) == 0 || (f(xmid)*f(x1) == 0)) % 중간값이 정확히 해일 때
-    disp("정확한 해를 찾았습니다!"); % 메시지 출력
-    disp("정확한 해 = " + xmid); % 정확한 해 출력
+    root = xmid;
+    err = 0;
     return; % 스크립트 실행 중지
 else  % 이도 저도 아니면 (=오류)
-    disp("오류 발생! x1과 x2를 제대로 입력해주세요."); % 메시지 출력
+    root = "err";
+    err = "err";
     return; % 스크립트 실행 중지
 end
 root = xmid; % 결과값 대입
